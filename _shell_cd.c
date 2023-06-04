88913eb5d97e2c21e1512ae36e9181c75c975393
patrice02 <patricedev02@gmail.com>
2023-06-04 23:34:18 +0000

    add builtin cd command

--- _shell_cd.c
+++ _shell_cd.c
@@ -8,42 +8,47 @@
  *
  * Return: 0 on sucess, 1 on failure
  */
-int change_directory(char *dir)
+int change_directory(char **args)
 {
+	char *dir = args[1];
+
 	char cwd[PATH_MAX];
+	char *av1[2] = {"OLDPWD", getenv("PWD")};
+	char *av2[2] = {"PWD", getcwd(cwd, sizeof(cwd))};
 
 	if (dir == NULL || *dir == '\0')
 	{
-		dir = _strdup(_get_env("HOME"));
+		dir = strdup(getenv("HOME"));
 		if (dir == NULL)
-			dir = _strdup(_get_env("PWD"));
+			dir = strdup(getenv("PWD"));
 	}
-	else if (_strcmp(dir, "-") == 0)
+	else if (strcmp(dir, "-") == 0)
 	{
-		dir = _strdup(_get_env("OLDPWD"));
+		dir = strdup(getenv("OLDPWD"));
 		if (dir == NULL)
-			dir = _strdup(_get_env("PWD"));
+			dir = strdup(getenv("PWD"));
 
-		print_str(dir);
-		print_str("\n");
+		print_stdout(dir);
+		print_stdout("\n");
 	}
 	else
-		dir = _strdup(dir);
-	if (_setenv("OLDPWD", _get_env("PWD")) == -1)
+		dir = strdup(dir);
+	
+	if (_setenv(av1) == -1)
 	{
-		print_err("Failed to update OLDPWD env variable");
+		print_stderror("Failed to update OLDPWD env variable");
 		return (2);
 	}
 	if (chdir(dir) == -1)
 	{
-		error_cd(dir);
-		free(dir);
+		// error_cd(dir);
+		// free(dir);
 		return (2);
 	}
-	free(dir);
-	if (_setenv("PWD", getcwd(cwd, sizeof(cwd))) == -1)
+	// free(dir);
+	if (_setenv(av2) == -1)
 	{
-		print_err("Failed to update PWD env variable");
+		print_stderror("Failed to update PWD env variable");
 		return (2);
 	}
 	return (0);
